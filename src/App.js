f5b02f96c48d0cffd8d138cc22370b248b390de5
ruchipatel40 <97028733+ruchipatel40@users.noreply.github.com>
2022-03-07 18:44:23 +0530

    Delete src directory

--- src/App.js
+++
@@ -1,4 +0,0 @@
-import { Other } from "./Other"
-import './style.css';
-console.warn(Other(),"App.js file");
-console.log(Other(),'second time App.js file');
--- src/Other.js
+++
@@ -1,3 +0,0 @@
-export function Other(){
-    return "Other function...";
-}
